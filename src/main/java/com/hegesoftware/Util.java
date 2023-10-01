20c298722c5871af97cf745976017f8ebcb68f1e
Henri Juntunen <henri.juntunen15@gmail.com>
2023-10-02 01:13:31 +0300

    Add cheat code.
    When player starts a game, the suffix of the name is
    checked against the string in RuleConfig.getCheatCode.
    If the two match, cheat mode will be activated, removing
    house edge.

--- src/main/java/com/hegesoftware/Util.java
+++ src/main/java/com/hegesoftware/Util.java
@@ -3,15 +3,29 @@ package com.hegesoftware;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Util {
+
+    private static int[] numArray;
 
     private static Scanner scanner = null;
 
     public static Scanner getScanner() {
         if (scanner == null) scanner = new Scanner(System.in);
         return scanner;
+    }
+
+    public static int[] getNumArray() {
+        if (numArray == null) {
+            numArray = new int[10];
+            for (int i = 1; i <= 10; i++) {
+                numArray[i - 1] = i;
+            }
+        }
+
+        return numArray;
     }
 }
